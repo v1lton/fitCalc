89443f99f7d2e85476a0cf8e985903d0f888bc2c
Wilton Ramos <wiltonramos@MacBook-Pro-de-Wilton.local>
2020-03-11 09:24:20 -0300

    Creating trainingSheetTableViewCell.swift

---
+++ primeiroLicativo/trainingSheetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  trainingSheetTableViewCell.swift
+//  primeiroLicativo
+//
+//  Created by Wilton Ramos on 11/03/20.
+//  Copyright © 2020 Wilton Ramos . All rights reserved.
+//
+
+import UIKit
+
+class trainingSheetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
