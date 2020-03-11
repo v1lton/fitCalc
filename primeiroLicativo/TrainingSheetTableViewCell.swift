404a8026ac84fa80368758c4cc52eaf5e147fdc0
Wilton Ramos <wiltonramos@MacBook-Pro-de-Wilton.local>
2020-03-11 09:34:56 -0300

    Mudei a posição dos texts fields

--- primeiroLicativo/TrainingSheetTableViewCell.swift
+++ primeiroLicativo/TrainingSheetTableViewCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class trainingSheetTableViewCell: UITableViewCell {
+class TrainingSheetTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
